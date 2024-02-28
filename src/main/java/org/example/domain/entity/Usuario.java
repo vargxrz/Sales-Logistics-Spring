0e29de56b5feef3c1247745bbfc699cc103e58dd
vargas <vargasvargasjoa@gmail.com>
2024-02-28 17:18:03 -0300

    spring

---
+++ src/main/java/org/example/domain/entity/Usuario.java
@@ -0,0 +1,32 @@
+package org.example.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column
+    @NotEmpty(message = "{campo.login.obrigatorio}")
+    private String login;
+    @Column
+    @NotEmpty(message = "{campo.senha.obrigatorio}")
+    private String senha;
+    @Column
+    private boolean admin;
+
+
+}
---
+++ src/main/java/org/example/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,36 @@
+package org.example.service.impl;
+
+import org.example.domain.entity.Usuario;
+import org.example.domain.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UsuarioServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByLogin(username).
+                orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado na base de dados."));
+
+        String[] roles = usuario.isAdmin() ? new String[]{"ADMIN", "USER"} : new String[]{"USER"};
+
+        return User.builder().username(usuario.getLogin()).password(usuario.getSenha()).roles(roles).build();
+    }
+}
